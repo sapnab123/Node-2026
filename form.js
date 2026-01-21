875bdd448e10d38161baa3fdf3f23dd291a8c78c
sapnab123 <sapnabindal20@gmail.com>
2026-01-21 13:43:57 +0530

    learn creating form in node

--- form.js
+++ form.js
@@ -1,5 +1,30 @@
 const http=require('http');
+const fs=require('fs');
 http.createServer((req,resp)=>{
-    resp.write(`<form></form>`);
-    resp.end();
+    fs.readFile('html/form.html','utf-8',(error,data)=>{
+        if(error){
+            resp.writeHead(500,{"content-type":"text/plain"});
+            resp.end("internal server erorr");
+            return;
+        }
+            if(req.url=='/'){
+    resp.write(data);}
+  else if(req.url=='/submit'){
+        resp.write("<h1>Data Submiddtted</h1>");
+  }
+  resp.end();    });
+    // resp.writeHead(200,{
+    //     "content-type":"text/html"
+    // });
+ 
+    // if(req.url=='/'){
+    // resp.write(`<form action="/submit" method="post">
+    //     <input  type="text" placeholder="enter name "  name="name">
+    //     <input  type="text" placeholder="enter name " name="email">
+    //     <button>Submit</button></form>`);
+    // }else if(req.url=='/submit'){
+    //     resp.write("<h1>Data Submitted</h1>");
+
+    // }
+    // resp.end();
 }).listen(3200);
